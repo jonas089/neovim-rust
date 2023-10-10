a0190c57a80ec8450032c25e065ea808eac39f2c
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2023-10-11 01:33:25 +0200

    rm example

--- nvim-rust-test/src/main.rs
+++
@@ -1,9 +0,0 @@
-extern crate rand;
-use rand::Rng;
-
-fn main() {
-    println!("Hello, world!");
-    let mut rng = rand::thread_rng();
-    let random_number = rng.gen_range(1..=100);
-    println!("Random number: {}", random_number);
-}
